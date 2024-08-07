21f68f8b3491c9d70c5378daee13c5b93dea4eec
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-08-07 09:12:40 +0200

    Create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import Home from "./pages/home/Home";
+import Location from "./pages/location/Location";
+
 // Styles
 import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/location" element={<Location />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/location/Location.jsx
@@ -0,0 +1,8 @@
+// Styles
+import "./location.css";
+
+export default function Location() {
+  return (
+    <div>Location</div>
+  )
+}
